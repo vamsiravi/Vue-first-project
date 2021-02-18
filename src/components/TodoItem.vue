aeda2424c195040ccb8d8b011943780560b7d41a
Vamsi Ravi <ravi.vamsi1995@gmail.com>
2021-02-18 23:32:20 +0100

    Updated methods

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p> <input type="checkbox" v-on:change="markComplete"> {{todo.title}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete (){
+            this.todo.completed = ! this.todo.completed;
+        }
+    } 
+
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+</style>
